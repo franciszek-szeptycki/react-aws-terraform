03c35ee458ec3c41d13ad0b75715ee007897aed5
franciszek-szeptycki <franciszek.szeptycki@gmail.com>
2024-03-16 23:06:07 -0400

    S# policy changes

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -9,3 +9,17 @@ resource "aws_s3_bucket_website_configuration" "static_website_bucket" {
     suffix = "index.html"
   }
 }
+
+resource "aws_s3_bucket_policy" "static_website_bucket" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.static_website_bucket.arn}/*"
+    }]
+  })
+}
