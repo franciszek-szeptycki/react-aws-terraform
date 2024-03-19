4cb043c652f73ea7fd0fe2b29e881d3ac403b307
franciszek-szeptycki <franciszek.szeptycki@gmail.com>
2024-03-19 00:44:00 -0400

    terraform fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,14 +26,3 @@ module "static_website" {
   bucket_name = "static-website-franeksz"
 }
 
-# #######################
-# #  CloudFront Module  #
-# #######################
-
-# module "cloudfront" {
-#   source = "./modules/cloudfront"
-
-#   s3_bucket_domain_name = "static-website-franeksz.s3-website.eu-central-1.amazonaws.com"
-#   acm_certificate_arn  = "arn:aws:acm:us-west-2:123456789012:certificate/abcd1234-abcd-1234-abcd-1234abcd1234"
-#   logging_bucket       = "aws-cloudtrail-logs-471112538384-7512a7cd"
-# }
